8fb2544adeb89592ec1f25f09afe2035c8a5996c
OXIDE <oxidiu777@gmail.com>
2023-07-01 21:51:51 +0300

    section5 done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import './App.scss';
-import HomePage from './pages/HomePage'; 
-import TradingReward from './pages/TradingReward';
+import HomePage from './pages/HomePage';
+import TradingReward from './pages/Trading-reward/TradingReward';
 import Pools from './pages/Pools/Pools';
 import PoolsHistory from './pages/Pools/History';
 import { Lottery } from './pages/Lottery/Lottery';
@@ -14,7 +14,7 @@ function App() {
       <Navbar />
         <Routes >
           <Route path="/" element={<HomePage />} />
-          <Route path="trading-reward" element={<TradingReward />} />
+          <Route path="/trading-reward" element={<TradingReward />} />
           <Route path="/pools" element={<Pools />} />
           <Route path="/pools/history" element={<PoolsHistory />} />
           <Route path="/lottery" element={<Lottery/>}/>
