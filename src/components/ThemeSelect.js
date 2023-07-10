e26fe93a731d8803a836cff2e5f45884080f047a
Oğuzhan Akçalı <oguzkhanakcali@gmail.com>
2023-07-10 16:05:14 +0300

    theme selection added to localstore

--- src/components/ThemeSelect.js
+++ src/components/ThemeSelect.js
@@ -3,10 +3,19 @@ import React from "react";
 const ThemeSelect = () => {
     const setDarkMode = () => {
         document.querySelector("body").setAttribute("data-theme", "dark");
+        localStorage.setItem("selectedTheme", "dark")
     };
     const setLightMode = () => {
         document.querySelector("body").setAttribute("data-theme", "light");
+        localStorage.setItem("selectedTheme", "light")
+
     };
+
+    const selectedTheme = localStorage.getItem("selectedTheme")
+
+    if (selectedTheme === "dark"){
+        setDarkMode();
+    }
 
     const toggleTheme = (e) => {
         if (e.target.checked) setDarkMode();
