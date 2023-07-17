263f16c20bbf83a55a146db466907fd2bace8a8f
Anna Zhykol <annzhy1309@gmail.com>
2023-07-17 19:29:39 +0300

    3

--- src/pages/Lottery/Lottery.js
+++ src/pages/Lottery/Lottery.js
@@ -85,67 +85,8 @@ export function Lottery(){
             <img src="assets/ticket-r.png" id="div3rightticket" className="div3img"/>
         </div>
         <div id="div4">
-          <div id="div4content">
              <h2 id="finishedround">Finished Rounds</h2>
-           <div id="div4switching">
-             <button id="allhistory"><b>All History</b></button>
-             <button id="yourhistory"><b>Your History</b></button>
-           </div>
-          <div id="historyframe">
-          <div id="allhistorytable">
-        <div id="tabletop">
-           <div id="tabletoplineleft">
-              <div id="tabletopline">
-                  <h2 id="round">Round</h2>
-                  <input pattern="^[0-9]+$" inputMode="numeric" id="round-id" name="round-id" scale="lg" className="sc-c22a9310-0 sc-82817dc1-0 hKHSKn lobcTU" value="877"/>
-               </div>
-               <div id="tabletopline2">
-                   "Drawn"
-                   "Apr 23, 2023, 2:00 AM"
-               </div>
-            </div>
-            <div id="tabletoplineright">
-                  <button className="arrow"><img src="assets/arrowleft.svg" alt=""/></button>
-                  <button className="arrow"><img src="assets/arrowright.svg" alt=""/></button>
-                  <button className="arrow"><img src="assets/arrowright2.svg" alt=""/></button>
-           </div>
-        </div>
-        <div id="tablecenter">
-               <div id="tablecentercontent">
-                   <div id="tablecentercontentline1">
-                       <h2 id="winningnumbers">Winning Number</h2>
-
-                           <div id="tablecentercontentline2">
-                           <div id="ballswithnumbers">
-                           <div>
-                               <img src="assets/ball1.svg" className="ballnumber" alt=""/>
-                           </div>
-                           <div >
-                               <img src="assets/ball2.svg"className="ballnumber" alt=""/>
-                           </div>
-                           <div >
-                               <img src="assets/ball3.svg"className="ballnumber" alt=""/>
-                           </div>
-                           <div >
-                               <img src="assets/ball4.svg"className="ballnumber" alt=""/>
-                           </div>
-                           <div >
-                               <img src="assets/ball5.svg"className="ballnumber" alt=""/>
-                           </div>
-                           <div >
-                               <img src="assets/ball6.svg"className="ballnumber" alt=""/>
-                           </div>
-                           </div>
-                        </div>
-                   </div>
-               </div>
-         </div>
-           <div id="tablebottom">
-              <ShowDetails/>
-           </div>
-  </div>
-          </div>
-          </div>
+           <SwitchHistory/>
         </div>
 
 
