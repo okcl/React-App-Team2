7708e82bda5fb8f0af334c255e72cc54bb5bfd46
Anna Zhykol <annzhy1309@gmail.com>
2023-07-18 13:35:58 +0300

    buy tickets

--- src/pages/Lottery/Lottery.js
+++ src/pages/Lottery/Lottery.js
@@ -1,4 +1,6 @@
 import TheCountUp from "../../components/CountUp";
+import { BuyTickets } from "./BuyTickets";
+import { BuyTicketsDiv1 } from "./BuyTicketsDiv1";
 import  ShowDetails  from "./ShowDetails";
 import ShowDetailsDiv2 from "./ShowDetailsDiv2";
 import { SwitchHistory } from "./SwitchHistory";
@@ -11,8 +13,8 @@ export function Lottery(){
 
            <div id="images11">
              <img src="assets/star-big.png" className="images1" id="bigstar" alt=""/>
-             <img src="assets/star-small.png" className="images1" id="smallstar" alt=""/>
-             <img src="assets/ticket-l.png" className="images1" id="ticketl" alt=""/>
+             <img src="assets/star-small.png" className="images1" id="smallstar"/>
+             <img src="assets/ticket-l.png" className="images1" id="ticketl"/>
            </div>
 
 
@@ -22,14 +24,14 @@ export function Lottery(){
               <p id="div1price">$<TheCountUp end={44294} duration={2}/></p>
               <h1 id="div1h12">in prizes!</h1>
               <div id="buyticketsbuttom">
-              <img src="assets/ticket.svg" id="backticket" alt=""/>
+              <img src="assets/ticket.svg" id="backticket"/>
               <button className="buttom" id="buyticketsbuttom1">Buy tickets</button>
               </div>
                </div>
 
            <div id="images12">
-             <img src="assets/three-stars.png" className="images1" id="threestars" alt=""/>
-             <img src="assets/ticket-r.png" className="images1" id="ticketr" alt=""/>
+             <img src="assets/three-stars.png" className="images1" id="threestars"/>
+             <img src="assets/ticket-r.png" className="images1" id="ticketr"/>
            </div>
 
         </div>
@@ -64,7 +66,7 @@ export function Lottery(){
                      </div>
                      <p id="yourticketstext">Your tickets</p>
                      <div id="prizepotbutton">
-                         <button className="buttom"><b>Buy Tickets</b></button>
+                         <BuyTickets/>
                      </div>
                  </div>
              </div>
@@ -76,7 +78,6 @@ export function Lottery(){
         </div>
         <div id="div3">
             <img src="assets/ticket-l.png" id="div3leftticket" className="div3img"/>
-
             <div id="div3content">
               <h2 className="textdiv3">Connect your wallet</h2>
               <h2 className="textdiv3">to check if you've won!</h2>
@@ -100,35 +101,35 @@ export function Lottery(){
              <div id="div5cards">
               <div id="div5card1">
                  <div className="div5line1">
-                     <p>Step 1</p>
+                     <p><b>Step 1</b></p>
                  </div>
                  <div className="div5line2">
-                     <h2>Buy tickets</h2>
+                     <h2><b>Buy tickets</b></h2>
                  </div>
                  <div className="div5line3">
-                     <p>Prices are set when the round starts, equal to 5 USD in CAKE per ticket.</p>
+                     <p><b>Prices are set when the round starts, equal to 5 USD in CAKE per ticket.</b></p>
                  </div>
               </div>
               <div id="div5card2">
                  <div className="div5line1">
-                     <p>Step 2</p>
+                     <p><b>Step 2</b></p>
                  </div>
                  <div className="div5line2">
-                     <h2>Wait for the Draw</h2>
+                     <h2><b>Wait for the Draw</b></h2>
                  </div>
                  <div className="div5line3">
-                     <p>There is one draw every day alternating between 0 AM UTC and 12 PM UTC.</p>
+                     <p><b>There is one draw every day alternating between 0 AM UTC and 12 PM UTC.</b></p>
                  </div>
               </div>
               <div id="div5card3">
                      <div className="div5line1">
-                     <p>Step 3</p>
+                     <p><b>Step 3</b></p>
                     </div>
                  <div className="div5line2">
-                     <h2>Check for Prizes</h2>
+                     <h2><b>Check for Prizes</b></h2>
                  </div>
                  <div className="div5line3">
-                     <p>Once the round’s over, come back to the page and check to see if you’ve won!</p>
+                     <p><b>Once the round’s over, come back to the page and check to see if you’ve won!</b></p>
                  </div>
               </div>
              </div>
@@ -136,55 +137,55 @@ export function Lottery(){
               <div id="div5text">
                 <div id="div5text1">
                   <div id="div5textpart">
-                  <div className="div5textheader"><h2>Winning criteria</h2></div>
+                  <div className="div5textheader"><h2><b>Winning criteria</b></h2></div>
                   <div className="div5textheader2">
-                  <h2>The digits on your ticket must match in the correct order to win.</h2>
+                  <h2><b>The digits on your ticket must match in the correct order to win.</b></h2>
                   </div>
                   <div className="div5p1">
-                      <p>Here’s an example lottery draw, with two tickets, A and B.</p>
+                      <p><b>Here’s an example lottery draw, with two tickets, A and B.</b></p>
                   </div>
                   <div className="div5p2">
                       <ul>
-                          <li>Ticket A: The first 3 digits and the last 2 digits match, but the 4th      digit is wrong, so this ticket only wins a “Match first 3” prize.</li>
-                          <li>Ticket B: Even though the last 5 digits match, the first digit is      wrong, so this ticket doesn’t win a prize.</li>
+                          <li><b>Ticket A: The first 3 digits and the last 2 digits match, but the 4th      digit is wrong, so this ticket only wins a “Match first 3” prize.</b></li>
+                          <li><b>Ticket B: Even though the last 5 digits match, the first digit is      wrong, so this ticket doesn’t win a prize.</b></li>
                       </ul>
                   </div>
                   <div className="div5p3">
-                      <p>Prize brackets don’t ‘stack’: if you match the first 3 digits in order,      you’ll only win prizes from the ‘Match 3’ bracket, and not from ‘Match 1’ and      ‘Match 2’.</p>
+                      <p><b>Prize brackets don’t ‘stack’: if you match the first 3 digits in order,      you’ll only win prizes from the ‘Match 3’ bracket, and not from ‘Match 1’ and      ‘Match 2’.</b></p>
                   </div>
                   </div>
                    <div id="div5table">
                       <div id="raw1">
-                          <img src="assets/div5ball1.svg" alt=""/>
-                          <img src="assets/div5ball2.svg" alt=""/>
-                          <img src="assets/div5ball3.svg" alt=""/>
-                          <img src="assets/div5ball4.svg" alt=""/>
-                          <img src="assets/div5ball5.svg" alt=""/>
-                          <img src="assets/div5ball6.svg" alt=""/>
+                          <img src="assets/div5ball1.svg"/>
+                          <img src="assets/div5ball2.svg"/>
+                          <img src="assets/div5ball3.svg"/>
+                          <img src="assets/div5ball4.svg"/>
+                          <img src="assets/div5ball5.svg"/>
+                          <img src="assets/div5ball6.svg"/>
                       </div>
                       <div id="raw2">
                           <h2 className="tableH2">A</h2>
-                          <img src="assets/div5raw2.svg" alt=""/>
+                          <img src="assets/div5raw2.svg"/>
                       </div>
                       <div id="raw3">
                           <h2 className="tableH2">B</h2>
-                          <img src="assets/div5raw3.svg" alt=""/>
+                          <img src="assets/div5raw3.svg"/>
                       </div>
                    </div>
                  </div>
              <hr/>
            <div id="div5text2">
              <div id="div5textpart2">
-             <div className="div5textheader"><h2>Prize Funds</h2></div>
+             <div className="div5textheader"><h2><b>Prize Funds</b></h2></div>
              <div className="div5p1">
-                 <p>The prizes for each lottery round come from three sources:</p>
+                 <p><b>The prizes for each lottery round come from three sources:</b></p>
              </div>
              <div className="div5textheader2">
-                 <h2>Ticket Purchases</h2>
+                 <h2><b>Ticket Purchases</b></h2>
              </div>
              <div className="div5p2">
                  <ul>
-                     <li>100% of the CAKE paid by people buying tickets that round goes back into the prize pools.</li>
+                     <li><b>100% of the CAKE paid by people buying tickets that round goes back into the prize pools.</b></li>
                  </ul>
              </div>
              <div className="div5textheader2">
@@ -192,25 +193,25 @@ export function Lottery(){
              </div>
              <div className="div5p2">
                  <ul>
-                     <li>After every round, if nobody wins in one of the prize brackets, the unclaimed CAKE for that bracket rolls over into the next round and are redistributed among the prize pools.</li>
+                     <li><b>After every round, if nobody wins in one of the prize brackets, the unclaimed CAKE for that bracket rolls over into the next round and are redistributed among the prize pools.</b></li>
                  </ul>
              </div>
              <div className="div5textheader2">
-                 <h2><div className="div5textheader2">CAKE Injections</div></h2>
+                 <h2><div className="div5textheader2"><b>CAKE Injections</b></div></h2>
              </div>
              <div className="div5p2">
                  <ul>
-                     <li>An average total of 35,000 CAKE from the treasury is added to lottery rounds over the course of a week. This CAKE is of course also included in rollovers! Read more in our guide to <a href="cake tokenomics" className="div5link">CAKE Tokenomics</a></li>
+                     <li><b>An average total of 35,000 CAKE from the treasury is added to lottery rounds over the course of a week. This CAKE is of course also included in rollovers! Read more in our guide to <a href="cake tokenomics" className="div5link">CAKE Tokenomics</a></b></li>
                  </ul>
              </div>Name
              </div>
              <div id="table2">
                  <div id="t2raw1">
-                     <img src="assets/div5table.svg" alt=""/>
+                     <img src="assets/div5table.svg"/>
                  </div>
                  <div id="t2raw2">
                      <div id="column1">
-                      <h5 className="columnheader">DIGITS MATCHED</h5>
+                      <h5 className="columnheader"><b>DIGITS MATCHED</b></h5>
                          <ul>
                              <li className="column1li"><div className="circle" id="c1"></div>Matches first 1</li>
                              <li className="column1li"><div className="circle" id="c2"></div>Matches first 2</li>
@@ -222,7 +223,7 @@ export function Lottery(){
                          </ul>
                      </div>
                      <div id="column2">
-                      <h5 className="columnheader">PRIZE POOL ALLOCATION</h5>
+                      <h5 className="columnheader"><b>PRIZE POOL ALLOCATION</b></h5>
                          <ul>
                              <li className="clumn2li">2%</li>
                              <li className="clumn2li">3%</li>
@@ -239,10 +240,10 @@ export function Lottery(){
            <hr/>
              </div>
            <div id="div5bottom">
-             <img src="assets/tombola.png" id="paintinrabbit" alt=""/>
+             <img src="assets/tombola.png" id="paintinrabbit"/>
              <div id="div5bottomtext">
-                 <h2>Still got questions?</h2>
-                 <p>Check our in-depth guide on <a href="link" className="div5link">how to play the PancakeSwap lottery!</a></p>
+                 <h2><b>Still got questions?</b></h2>
+                 <p><b>Check our in-depth guide on <a href="link" className="div5link">how to play the PancakeSwap lottery!</a></b></p>
              </div>
             </div>
            </div>
