e56d48be4f80562d44c242add0725c53d1990b8e
OXIDE <oxidiu777@gmail.com>
2023-07-19 19:51:04 +0300

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -20,7 +20,7 @@ function HomePage() {
                                             <path d="M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z"></path>
                                         </svg>
                                     </button>
-                                </a> 
+                                </a>
                             </div>
                             <span className="half-coin-span1 slide-1">
                                     <img src="https://pancakeswap.finance/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fv3BgCoinDown.45ce8443.png&w=256&q=75" alt="half-coin" className="half-coin" />
@@ -47,7 +47,12 @@ function HomePage() {
                             </div>
                         </div>
                     </div>
-
+                    <div class="part1SVGContainer">
+                        <div class="part1SVGContainerChild">
+                            <svg className='part1SvgDark' viewBox="0 0 1660 339" class="sc-231a1e38-0 dPwWVs slide-svg-dark" width="100%" color="text" xmlns="http://www.w3.org/2000/svg"><path d="M804 166.523C520.5 166.523 267.5 290.022 0 304V338.5H1660V0C1358.83 0 1104 166.523 804 166.523Z" fill="url(#paint0_linear_dark)"></path><defs><linearGradient id="paint0_linear_dark" x1="830" y1="83.5" x2="830" y2="338.5" gradientUnits="userSpaceOnUse"><stop stop-color="#08060B" stop-opacity="0.2"></stop><stop offset="0.545554" stop-color="#08060B" stop-opacity="0.5"></stop><stop offset="1" stop-color="#08060B"></stop></linearGradient></defs></svg>
+                            <svg className='part1SvgLight' viewBox="0 0 1660 339" class="sc-231a1e38-0 dPwWVs slide-svg-light" width="100%" color="text" xmlns="http://www.w3.org/2000/svg"><path d="M804 167.023C520.5 167.023 267.5 290.522 0 304.5V339H1660V0.5C1358.83 0.5 1104 167.023 804 167.023Z" fill="url(#paint0_linear_light)"></path><defs><linearGradient id="paint0_linear_light" x1="830" y1="84" x2="830" y2="339" gradientUnits="userSpaceOnUse"><stop stop-color="white" stop-opacity="0.48"></stop><stop offset="0.566389" stop-color="white" stop-opacity="0.35"></stop><stop offset="1" stop-color="white"></stop></linearGradient></defs></svg>
+                        </div>
+                    </div>
                     <div id="homepage-hero" >
                         <div id="homepage-hero-left">
                             <h2 className="homepage-hero-left-text-1">The moon is made of pancakes.</h2>
@@ -306,8 +311,8 @@ function HomePage() {
                                             <div className="card-left-content">
                                                 <div>Prediction</div>
                                                 <h2>$1.2 billion</h2>
-                                                <div>in BNB + CAKE won so far</div> 
-                                                <div>Predict the price trend of BNB or CAKE to win</div>   
+                                                <div>in BNB + CAKE won so far</div>
+                                                <div>Predict the price trend of BNB or CAKE to win</div>
                                             </div>
                                             <div className="card-left-button">
                                                 <Link to="/">
