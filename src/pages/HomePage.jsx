a1b059cc856527be2cb87803c52e5e6963f26a85
OXIDE <oxidiu777@gmail.com>
2023-07-19 21:00:43 +0300

    done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -47,10 +47,10 @@ function HomePage() {
                             </div>
                         </div>
                     </div>
-                    <div class="part1SVGContainer">
-                        <div class="part1SVGContainerChild">
-                            <svg className='part1SvgDark' viewBox="0 0 1660 339" class="sc-231a1e38-0 dPwWVs slide-svg-dark" width="100%" color="text" xmlns="http://www.w3.org/2000/svg"><path d="M804 166.523C520.5 166.523 267.5 290.022 0 304V338.5H1660V0C1358.83 0 1104 166.523 804 166.523Z" fill="url(#paint0_linear_dark)"></path><defs><linearGradient id="paint0_linear_dark" x1="830" y1="83.5" x2="830" y2="338.5" gradientUnits="userSpaceOnUse"><stop stop-color="#08060B" stop-opacity="0.2"></stop><stop offset="0.545554" stop-color="#08060B" stop-opacity="0.5"></stop><stop offset="1" stop-color="#08060B"></stop></linearGradient></defs></svg>
-                            <svg className='part1SvgLight' viewBox="0 0 1660 339" class="sc-231a1e38-0 dPwWVs slide-svg-light" width="100%" color="text" xmlns="http://www.w3.org/2000/svg"><path d="M804 167.023C520.5 167.023 267.5 290.522 0 304.5V339H1660V0.5C1358.83 0.5 1104 167.023 804 167.023Z" fill="url(#paint0_linear_light)"></path><defs><linearGradient id="paint0_linear_light" x1="830" y1="84" x2="830" y2="339" gradientUnits="userSpaceOnUse"><stop stop-color="white" stop-opacity="0.48"></stop><stop offset="0.566389" stop-color="white" stop-opacity="0.35"></stop><stop offset="1" stop-color="white"></stop></linearGradient></defs></svg>
+                    <div className="part1SVGContainer">
+                        <div className="part1SVGContainerChild">
+                            <svg className='part1SvgDark' viewBox="0 0 1660 339" width="100%" color="text" xmlns="http://www.w3.org/2000/svg"><path d="M804 166.523C520.5 166.523 267.5 290.022 0 304V338.5H1660V0C1358.83 0 1104 166.523 804 166.523Z" fill="url(#paint0_linear_dark)"></path><defs><linearGradient id="paint0_linear_dark" x1="830" y1="83.5" x2="830" y2="338.5" gradientUnits="userSpaceOnUse"><stop stop-color="#08060B" stop-opacity="0.2"></stop><stop offset="0.545554" stop-color="#08060B" stop-opacity="0.5"></stop><stop offset="1" stop-color="#08060B"></stop></linearGradient></defs></svg>
+                            <svg className='part1SvgLight' viewBox="0 0 1660 339" width="100%" color="text" xmlns="http://www.w3.org/2000/svg"><path d="M804 167.023C520.5 167.023 267.5 290.522 0 304.5V339H1660V0.5C1358.83 0.5 1104 167.023 804 167.023Z" fill="url(#paint0_linear_light)"></path><defs><linearGradient id="paint0_linear_light" x1="830" y1="84" x2="830" y2="339" gradientUnits="userSpaceOnUse"><stop stop-color="white" stop-opacity="0.48"></stop><stop offset="0.566389" stop-color="white" stop-opacity="0.35"></stop><stop offset="1" stop-color="white"></stop></linearGradient></defs></svg>
                         </div>
                     </div>
                     <div id="homepage-hero" >
